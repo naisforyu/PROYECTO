8c0bab6d5d67ae8fa3b77768b2d5979133016884
naisforyu <rourca27@gmail.com>
2025-11-17 13:16:28 -0500

    2.9

---
+++ loader.js
@@ -0,0 +1,8 @@
+    window.addEventListener("load", () => {
+        const loader = document.getElementById("loader");
+        loader.classList.add("hidden");
+
+        setTimeout(() => {
+            loader.style.display = "none";
+        }, 500);
+    });
